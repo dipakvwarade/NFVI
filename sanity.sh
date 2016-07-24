bd63aa78be91e45ba7da97e99f19027f12eac44f
Dipak Warade <dipakvwarade@gmail.com>
2016-07-24 11:59:24 +0400

    added color

--- sanity.sh
+++ sanity.sh
@@ -5,7 +5,8 @@
 #Date		: 24/07/2016
 #Disclaimer	:Your use of the script,is at your sole risk. All information on these pages is provided "as -is", without any warranty. You may modify this script as per your requirement. should not be used without understanding of your environement and script functionality.
 ####################################
-
+RED='\033[0;31m'
+NC='\033[0m' # No Color
 echo "################################### SUMMERY #################################"
 CPU=`top -b -n1 | grep "Cpu(s)" | awk '{print $2 + $4}'`
 FREE_MEM=`free -m | grep Mem`
@@ -20,9 +21,14 @@ HDD=`df -lh | awk '{if ($6 == "/") { print $5 }}' | head -1 | cut -d'%' -f1`
 MOUNT_RO=$(if mount|grep  -v rw >/tmp/ro_fs; then  echo "RO Mounts found check /tmp/ro_fs and investigate"; else echo "No Read Only Mounts Found"; fi)
 LOAD=$(uptime|awk '{print $10, $11 ,$12}')
 LINK_DOWN=$(ip link show|grep -i DOWN|wc -l)
-echo "CURRENT SWAP % : $SWAP"
-echo "CURRENT RAM % : $RAM"
-echo "HDD %: $HDD"
-echo $MOUNT_RO
-echo "Current Load on the System 1min,5min,15min : $LOAD"
-echo "TOTAL Links Down,Investigate if more than 0 : $LINK_DOWN"
+printf "CURRENT SWAP "\:" ${RED} $SWAP ${NC}\n"
+printf "CURRENT root partition "\:" ${RED} $HDD ${NC}\n"
+printf "CURRENT RAM "\:" ${RED} $RAM ${NC}\n"
+printf "Read Only Mounts "\:" ${RED} $MOUNT_RO ${NC}\n"
+printf "Current Load on the System 1min,5min,15min "\:" ${RED} $LOAD ${NC}\n"
+printf "TOTAL Links Down,Investigate if more than 0 "\:" ${RED} $LINK_DOWN ${NC}\n"
+#echo "CURRENT RAM % : $RAM"
+#echo "HDD %: $HDD"
+#echo $MOUNT_RO
+#echo "Current Load on the System 1min,5min,15min : $LOAD"
+#echo "TOTAL Links Down,Investigate if more than 0 : $LINK_DOWN"
