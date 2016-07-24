66c473e880187a578f052490d6646a8ea5ecd7a0
Dipak Warade <dipakvwarade@gmail.com>
2016-07-24 11:28:58 +0400

    added sanity check script

---
+++ sanity.sh
@@ -0,0 +1,28 @@
+#!/bin/bash 
+####################################
+#Function 	: Sanity checks on Ubuntu 14.04
+#Auther 	: Dipak Warade
+#Date		: 24/07/2016
+#Disclaimer	:Your use of the script,is at your sole risk. All information on these pages is provided "as -is", without any warranty. You may modify this script as per your requirement. should not be used without understanding of your environement and script functionality.
+####################################
+
+echo "################################### SUMMERY #################################"
+CPU=`top -b -n1 | grep "Cpu(s)" | awk '{print $2 + $4}'`
+FREE_MEM=`free -m | grep Mem`
+FREE_SWAP=`free -m | grep Swap`
+CURRENT=`echo $FREE_MEM | cut -f3 -d' '`
+CURRENT_SWAP=`echo $FREE_SWAP | cut -f3 -d' '`
+TOTAL=`echo $FREE_MEM | cut -f2 -d' '`
+TOTAL_SWAP=`echo $FREE_SWAP | cut -f2 -d' '`
+SWAP=$(echo "scale = 2; $CURRENT_SWAP/$TOTAL_SWAP*100" | bc)
+RAM=$(echo "scale = 2; $CURRENT/$TOTAL*100" | bc)
+HDD=`df -lh | awk '{if ($6 == "/") { print $5 }}' | head -1 | cut -d'%' -f1`
+MOUNT_RO=$(if mount|grep  -v rw >/tmp/ro_fs; then  echo "RO Mounts found check /tmp/ro_fs and investigate"; else echo "No Read Only Mounts Found"; fi)
+LOAD=$(uptime|awk '{print $10, $11 ,$12}')
+LINK_DOWN=$(ip link show|grep -i DOWN|wc -l)
+echo "CURRENT SWAP % : $SWAP"
+echo "CURRENT RAM % : $RAM"
+echo "HDD %: $HDD"
+echo $MOUNT_RO
+echo "Current Load on the System 1min,5min,15min : $LOAD"
+echo "TOTAL Links Down,Investigate if more than 0 : $LINK_DOWN"
